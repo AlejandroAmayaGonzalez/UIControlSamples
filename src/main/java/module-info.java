967f342565881226751b52d0a3df2aa6c67244c0
AlejandroAmayaGonzalez <132985108+AlejandroAmayaGonzalez@users.noreply.github.com>
2024-12-03 17:37:47 +0100

    Add files via upload

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module ieslosmontecillos.uicontrolsample {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    exports ieslosmontecillos.uicontrolsample.tooltipsample;
+    exports ieslosmontecillos.uicontrolsample.checkboxsample;
+    exports ieslosmontecillos.uicontrolsample.tableviewsample;
+    exports ieslosmontecillos.uicontrolsample.listviewsample;
+}
